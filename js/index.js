cea2599117f24e716b39a63bbefc96a74a087daa
fredosanto <93183340+fredosanto@users.noreply.github.com>
2022-10-29 23:06:50 +0200

    added featured items in index

--- js/index.js
+++ js/index.js
@@ -1,80 +1,29 @@
-import { getInCart } from "./utils/addToCart.js";
-
-
+const url = "https://fredo.one/rainy_days/wp-json/wc/store/products/?featured=true";
 const bestProductContainer = document.querySelector(".best-products_container");
 
-const cartItems = getInCart();
+async function getFeaturedJackets() {
+    try {
+        const response = await fetch(url);
+        const featuredJackets = await response.json();
 
-jackets.forEach((jacket) => {
+        console.log(featuredJackets);
 
-
-    let cssClass = "fas";
-
-    const isJacketIncluded = cartItems.find(function(added) {
-        console.log(added)
-
-        return added.name === jacket.name;
-    })
-
-    // console.log(isJacketIncluded);
-
-    if(isJacketIncluded) {
-        cssClass = "far";
+        featuredJackets.forEach(function(jacket) {
+            bestProductContainer.innerHTML += `<div class="best-product"
+                                                    <div>
+                                                        <a href="/details.html?id=${jacket.id}">
+                                                            <img class="best-product_img" src="${jacket.images[0].src}" alt="">
+                                                        </a>
+                                                        <p>${jacket.name}</p>
+                                                        <p>${jacket.prices.price} ${jacket.prices.currency_code}</p>
+                                                    </div>
+                                                </div>`
+        });
     }
 
-
-    bestProductContainer.innerHTML += `<div class="best-product"
-                                            <div>
-                                                <a href="/jacket.html">
-                                                    <img class="best-product_img" src="${jacket.imgFile}">
-                                                </a>
-                                                <p>${jacket.name}</p>
-                                                <p>${jacket.price}</p>
-                                                <div class="add-buttons">
-                                                    <i class="${cssClass} fa-cart-shopping add-cart" data-name="${jacket.name}" data-price="${jacket.price}"></i>
-                                                </div>
-                                            </div>
-                                        </div>`
-});
-
-
-const addCartButtons = document.querySelectorAll(".add-buttons i");
-
-addCartButtons.forEach((button) => {
-    button.addEventListener("click", handleClick);
-});
-
-function handleClick() {
-    // console.log(event);
-    this.classList.toggle("far");
-    this.classList.toggle("fas");
-    
-    const jacketName = this.dataset.name;
-    const jacketPrice = this.dataset.price;
-    // console.log("name", jacketName);
-
-    const currentCart = getInCart();
-    // console.log(currentCart);
-
-
-    const itemInCart = currentCart.find(function(added) {
-        return added.name === jacketName;
-    });
-    
-    if (itemInCart === undefined) {
-        const item = { name: jacketName, price: jacketPrice };
-        currentCart.push(item);
-        saveItem(currentCart);
-    } else {
-        const newCart = currentCart.filter(added => added.name !== jacketName);
-        saveItem(newCart);
+    catch(error) {
+        console.log(error);
     }
-
-
-
-};
-
-
-function saveItem(cart) {
-    localStorage.setItem("cartItems", JSON.stringify(cart));
 }
+
+getFeaturedJackets()
